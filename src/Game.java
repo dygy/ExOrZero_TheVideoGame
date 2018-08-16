01c04c8ef0de0438ba1468e9d148063ea24293c4
Dygy <Yukimurachris@gmail.com>
2018-08-16 14:26:47 +0300

    There is some bugs, it needs to sometimes turn 4-5 times, but this patch makes imposable to chose numbers bigger than 9, select 0 , or already selected number. The cheks algorithms aren't good enough, but with bugs it works.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,6 @@
 import org.apache.commons.lang3.ArrayUtils;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 class Game {
     static int turn;
@@ -6,6 +8,8 @@ class Game {
     static int turnNum = 0;
     private static final Scanner scanner = new Scanner(System.in);
     private static int[] chose = new int[9];
+    private static int select = scanner.nextInt();
+    private static List<Integer> list = new ArrayList<>();
     static void craftChose(){
         int j=1;
         for(int i = 0; i< chose.length; i++){
@@ -43,49 +47,60 @@ class Game {
 
         }
     }
-    static void turning(){
+    static boolean notBig=false;
+    static boolean notAlreadySelected=false;
+    static void turning() {
+        if (select==0){
+            select=100;
+        }
+        chekIfBig();
+        notBig=true;
+        chekIfAlreadySelected();
+        notAlreadySelected =true;
+        if (notAlreadySelected && notBig) {
+            list.add(select);
+            chose = ArrayUtils.removeElement(chose, select);
 
-        int select = scanner.nextInt();
-        chose =  ArrayUtils.removeElement(chose, select);
-        if (select == 1){
-            Matrix.i = 0;
-            Matrix.j = 0;
+            if (select == 1) {
+                Matrix.i = 0;
+                Matrix.j = 0;
+
+            }
+            if (select == 2) {
+                Matrix.i = 0;
+                Matrix.j = 1;
+            }
+            if (select == 3) {
+                Matrix.i = 0;
+                Matrix.j = 2;
+            }
+            if (select == 4) {
+                Matrix.i = 1;
+                Matrix.j = 0;
+            }
+            if (select == 5) {
+                Matrix.i = 1;
+                Matrix.j = 1;
+            }
+            if (select == 6) {
+                Matrix.i = 1;
+                Matrix.j = 2;
+            }
+            if (select == 7) {
+                Matrix.i = 2;
+                Matrix.j = 0;
+            }
+            if (select == 8) {
+                Matrix.i = 2;
+                Matrix.j = 1;
+            }
+            if (select == 9) {
+                Matrix.i = 2;
+                Matrix.j = 2;
+            }
+            Matrix.changeMatrix(Matrix.i, Matrix.j);
 
         }
-        if (select == 2){
-            Matrix.i = 0;
-            Matrix.j = 1;
-        }
-        if (select == 3){
-            Matrix.i = 0;
-            Matrix.j = 2;
-        }
-        if (select == 4){
-            Matrix.i = 1;
-            Matrix.j = 0;
-        }
-        if (select == 5){
-            Matrix.i = 1;
-            Matrix.j = 1;
-        }
-        if (select == 6){
-            Matrix.i = 1;
-            Matrix.j = 2;
-        }
-        if (select == 7){
-            Matrix.i = 2;
-            Matrix.j = 0;
-        }
-        if (select == 8){
-            Matrix.i = 2;
-            Matrix.j = 1;
-        }
-        if (select == 9){
-            Matrix.i = 2;
-            Matrix.j = 2;
-        }
-        Matrix.changeMatrix(Matrix.i,Matrix.j);
-
     }
     static void checkWin(){
         boolean xWon = Matrix.s[0][0].equals("Х") && Matrix.s[0][1].equals("Х") && Matrix.s[0][2].equals("Х") || Matrix.s[0][1].equals("X") && Matrix.s[1][1].equals("X") && Matrix.s[2][1].equals("X") ||
@@ -112,4 +127,38 @@ class Game {
 
         }
     }
+    public static void chekIfBig() {
+        while (select>9) {
+            System.out.println("Select correct value");
+            select = scanner.nextInt();
+
+            int chek;
+            for (Integer aList : list) {
+                chek = aList;
+                do {
+
+                    chekIfAlreadySelected();
+                } while (select == chek);
+            }
+
+        }
+    }
+    public static void chekIfAlreadySelected(){
+        int chek;
+        for (Integer aList : list) {
+            chek = aList;
+            do {
+                System.out.println("Select another value");
+                select = scanner.nextInt();
+                if (select>9) {
+
+                    chekIfBig();
+                }
+            } while (select == chek);
+
+        }
+
+
+    }
+
 }
